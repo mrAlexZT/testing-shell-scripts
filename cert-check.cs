663440b6b69aee2926ffb9072b5207380c9db004
Oleksandr Shevchenko <mr.alexzt@gmail.com>
2018-07-18 22:26:13 +0300

    add cert-check.cs

--- cert-check.cs
+++ cert-check.cs
@@ -3,30 +3,24 @@ import javax.net.ssl.HttpsURLConnection;
 import java.security.cert.X509Certificate;
 import java.security.cert.Certificate;
 
-var respCheck = checkCertificate("https://11111.demo.jelastic.com/");
-if (respCheck.result != 0) {
-	respCheck.step = 'checkCertificate';
-	return respCheck;
-}
-
-function checkCertificate(url) {
-	var res = [];
-	var destinationURL = new URL(url);
-	var conn = destinationURL.openConnection();
-	conn.connect();
-	var certs = conn.getServerCertificates();
-	for (var i = 0; i < certs.length; i++) {
-		var cert = certs[i];
-		var issuer = cert.getIssuerDN().toString();
-		if (issuer.indexOf("Fake LE") > -1) return {
-			result: 0,
-			message: "passed"
-		}
-
+var res = [];
+var destinationURL = new URL("https://11111.demo.jelastic.com/");
+var conn = destinationURL.openConnection();
+conn.connect();
+var certs = conn.getServerCertificates();
+for (var i = 0; i < certs.length; i++) {
+	var cert = certs[i];
+	var issuer = cert.getIssuerDN().toString();
+	if (issuer.indexOf("Fake LE") > -1) return {
+		result: 0,
+		message: "passed"
 	}
-	return {
-		result: 99,
-		error: "Can't find LE certificate"
-	};
+	else {
+		return {
+			result: 99,
+			error: "Can't find LE certificate"
+		}
+	}
 }
+
 
